87d328ab55adedfd75814911b716b22f09f7412b
Reversive <matiaspavan@gmail.com>
2025-05-06 22:48:39 -0300

    feat: added tests for router

--- internal/router/router_test.go
+++ internal/router/router_test.go
@@ -1,11 +1,41 @@
 package router
 
 import (
+	"net/http"
+	"net/http/httptest"
 	"testing"
 )
 
-func RouterTest(t *testing.T) {
+func TestRouter(t *testing.T) {
 	t.Run("allowed method", func(t *testing.T) {
+		r := NewRouter()
+		r.Get("/get", func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(http.StatusOK)
+		})
 
+		req, _ := http.NewRequest(http.MethodGet, "/get", nil)
+		rr := httptest.NewRecorder()
+		r.Handler.ServeHTTP(rr, req)
+		if status := rr.Code; status != http.StatusOK {
+			t.Errorf("handler returned wrong status code: got %v but want %v",
+				status, http.StatusOK)
+		}
+	})
+
+	t.Run("not allowed method", func(t *testing.T) {
+		r := NewRouter()
+
+		r.Get("/get", func(w http.ResponseWriter, r *http.Request) {
+		})
+
+		req, _ := http.NewRequest(http.MethodPost, "/get", nil)
+		rr := httptest.NewRecorder()
+
+		r.Handler.ServeHTTP(rr, req)
+
+		if status := rr.Code; status != http.StatusMethodNotAllowed {
+			t.Errorf("request returned wrong status code, got %v but want %v",
+				status, http.StatusMethodNotAllowed)
+		}
 	})
 }
--- internal/services/novel_service.go
+++ internal/services/novel_service.go
@@ -3,11 +3,11 @@ package services
 import "github.com/Reversive/jindo/internal/repositories"
 
 type NovelService struct {
-	novelRepository repositories.NovelRepository
+	NovelRepository repositories.NovelRepository
 }
 
 func NewNovelService(
 	novelRepository repositories.NovelRepository,
 ) *NovelService {
-	return &NovelService{novelRepository: novelRepository}
+	return &NovelService{NovelRepository: novelRepository}
 }
